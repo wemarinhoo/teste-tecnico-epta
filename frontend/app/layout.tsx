7349a1763820ba7c4e1919f21b4ba8ee0a2c6d11
Wendell Marinho <wtmarinho55@gmail.com>
2025-08-03 18:11:29 -0300

    first commit

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Epta Softwares",
+  description: "Epta Softwares",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body
+        className={`${inter.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
